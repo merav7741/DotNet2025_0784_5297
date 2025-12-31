db2d3af02076f027d88ecb002889bd8ea7dd755c
מירב יאיר <MeravYair@smg.local>
2025-12-31 16:14:47 +0200

    programStart2

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -8,15 +8,15 @@ namespace DalTest
     internal class Program
     {
         static readonly IDal s_dal = new Dal.DalList();
-       
+
         private static void Main(string[] args)
         {
             try
             {
-                Console.WriteLine("שלום ןברכה");
+
                 Initialization.Initialize(s_dal);
 
-               
+
                 //SubMenu();
             }
             catch (Exception ex)
@@ -24,49 +24,135 @@ namespace DalTest
                 Console.WriteLine($"Error: {ex.Message}");
             }
         }
-        //private static void SubMenu<T>(ICrud<T> crud, string title, Add add, Update update) { }
-
-        private static void ProductMenu() { }
-
-        //private static Sale AskSale(int code = 0) { }
-
-        //private static Client AskClient(int identity = 0) { }
-
-        private static void AddProduct() { }
-
-        private static void AddSale() { }
-
-        private static void AddClient() { }
-
-        private static void UpdateProduct() { }
-
-        private static void UpdateSale() { }
-
-        private static void UpdateClient() { }
-
-        private static void ReadAll<T>(List<T> list) { }
-
-        //private static void ReadAll<T>(ICrud<T> icrud) { }
 
         private static void Read<T>(ICrud<T> crud)
         {
+            int choice;
             try
             {
-                Console.WriteLine("Enter Code");
-                int code = int.Parse(Console.ReadLine());
-                Console.WriteLine(crud.Read(code));
-
+                while ((choice = PrintMainMenu()) != 0)
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            ProductMenu();
+                            break;
+                        case 2:
+                            SaleMenu();
+                            break;
+                        case 3:
+                            CustomerMenu();
+                            break;
+                    }
+                }
             }
-            catch (Exception e) { Console.WriteLine(e.Message); }
+            catch (Exception e)
+            { Console.WriteLine(e.Message); }
 
 
         }
 
+        public static int PrintMainMenu()
+        {
+            int choice;
+            Console.WriteLine("Main Menu: 1 - Product 2 - Sales 3 - Customers 0 - Exit");
+            int.TryParse(Console.ReadLine(), out choice);
+            return choice;
+        }
+
+        public static int PrintSubMenu(string item)
+        {
+            int choice;
+            Console.WriteLine($"{item} Menu: 1 - Add 2 - Update 3 - Read 4- Read All 5 - Delete 0 - Back");
+            int.TryParse(Console.ReadLine(), out choice);
+            return choice;
+        }
+        public static void ProductMenu()
+        {
+            int choice;
+            do
+            {
+                choice = PrintMainMenu();
+                switch (choice)
+                {
+                    case 1:
+                        AddProduct();
+                        break;
+                    case 2:
+                        UpdateProduct();
+                        break;
+                        case 3:
+                        Read(s_dal.Product!);
+                        break ;
+                        case 4:
+                        ReadAll(s_dal.Product!);
+                        break ;
+                        case 5:
+                         Delete(s_dal.Product);
+                        break ;
+
+
+                }
+              
+            } while (choice != 0);
+
+        } 
+        public static void CustomerMenu()
+        {
+
+        }
+        public static void SaleMenu()
+        {
+            int choice;
+            do
+            {
+                choice = PrintMainMenu();
+                switch (choice)
+                {
+                    case 1:
+                        AddSale();
+                        break;
+                    case 2:
+                        UpdateSale();
+                        break;
+                    case 3:
+                        Read(s_dal.Sale!);
+                        break;
+                    case 4:
+                        ReadAll(s_dal.Sale!);
+                        break;
+                    case 5:
+                        Delete(s_dal.Sale!);
+                        break;
+
+
+                }
+
+            } while (choice != 0);
+
+        }
         private static void Delete<T>(ICrud<T> crud) { }
 
-        //public static int PrintMainMenu() { }
+        private static void AddProduct()
+        {
 
-        //public static int PrintSubMenu(string item) { }
+        }
+        private static void AddSale()
+        {
+
+        }
+        private static void UpdateProduct() { }
+        private static void UpdateSale()
+        {
+
+        }
+        private static void ReadAll<T>(ICrud<T> icrud)
+        {
+            foreach (T item in icrud.ReadAll())
+                if (item != null)
+                    Console.WriteLine(item);
+        }
+        
 
         private static Product AskProduct(int code = 0)
         {
