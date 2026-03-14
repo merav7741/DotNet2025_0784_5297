7e6ca251d380b61d0d2ef7f6c650fddfa53df9ee
merav <my0583257741@gmail.com>
2026-03-15 00:06:59 +0200

    level 9 -5 4

--- DalXml/Config.cs
+++ DalXml/Config.cs
@@ -3,11 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace DalXml
 {
     internal class Config
     {
-        private string dataConfig;
+        private string configurationFileName = "../xml/data-config.xml";
+        public static int ProductNum
+        {
+            get
+            {
+                XElement file = XElement.Load(configurationFileName);
+                XElement curProdNum = file.Element("ProductNum");
+                int num = int.Parse(curProdNum.Value);
+                curProdNum.SetValue(num + 1);
+                file.Save(configurationFileName);
+                return num;
+            }
+        }
+        public static int SaleNum
+        {
+            get
+            {
+                XElement file = XElement.Load(configurationFileName);
+                XElement curSaleNum = file.Element("SaleNum");
+                int num = int.Parse(curSaleNum.Value);
+                curSaleNum.SetValue(num + 1);
+                file.Save(configurationFileName);
+                return num;
+            }
+        }
     }
 }
